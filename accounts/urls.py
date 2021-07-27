c24db91481f21f26c8b2ea5348d0dd566021de12
“Rohan-Darji” <rohandarji2001@gmail.com>
2021-07-27 21:00:27 +0530

    auth pages redesigned

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('accounts/login', views.LoginPage, name='Login'),
-    path('accounts/signup', views.SignupPage, name='Signup'),
+    path('accounts/signup', views.SignupPage, name='account_signup'),
     path('accounts/logout', views.LogoutPage, name='Logout'),
 ]
